d4d88e835924fe2ab4fdd0ed59dbc9add31091c2
Imad Rajab <imadrajab@gmail.com>
2014-12-23 20:28:14 +0200

    second draft

--- src/services/ReputationWS.php
+++ src/services/ReputationWS.php
@@ -33,27 +33,97 @@ class ApplicationWS extends Vbout {
 	
 	public function getOnlineReviews($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'online.'.$this->api_response;
+		
+		try {
+			$reviews = $this->online($params);
+
+            if ($reviews != null && isset($reviews['response'])) {
+                $result = array_merge($result, $reviews['data']['reviews']['items']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function getFeedback($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'getfeedack.'.$this->api_response;
+		
+		try {
+			$review = $this->getfeedack($params);
+
+            if ($review != null && isset($review['response'])) {
+                $result = array_merge($result, $review['data']['item']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function getSentiments($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'sentiment.'.$this->api_response;
+		
+		try {
+			$sentiment = $this->sentiment($params);
+
+            if ($sentiment != null && isset($sentiment['response'])) {
+                $result = array_merge($result, $sentiment['data']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function getStats($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'stats.'.$this->api_response;
+		
+		try {
+			$stats = $this->stats($params);
+
+            if ($stats != null && isset($stats['response'])) {
+                $result = array_merge($result, $stats['data']['stats']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function deleteFeedback($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'deletefeedback.'.$this->api_response;
+		
+		try {
+			$this->deletefeedback($params);
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 }
--- src/services/SocialMediaWS.php
+++ src/services/SocialMediaWS.php
@@ -13,31 +13,111 @@ class ApplicationWS extends Vbout {
 	
 	public function getCalendar($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'calendar.'.$this->api_response;
+		
+		try {
+			$calendar = $this->calendar($params);
+
+            if ($calendar != null && isset($calendar['response'])) {
+                $result = array_merge($result, $calendar['data']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function getChannels($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'channels.'.$this->api_response;
+		
+		try {
+			$channels = $this->channels($params);
+
+            if ($channels != null && isset($channels['response'])) {
+                $result = array_merge($result, $channels['data']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function getPost($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'getpost.'.$this->api_response;
+		
+		try {
+			$post = $this->getpost($params);
+
+            if ($post != null && isset($post['response'])) {
+                $result = array_merge($result, $post['data']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function getStats($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'stats.'.$this->api_response;
+		
+		try {
+			$stats = $this->stats($params);
+
+            if ($stats != null && isset($stats['response'])) {
+                $result = array_merge($result, $stats['data']);
+            }
+
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function deletePost($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'deletepost.'.$this->api_response;
+		
+		try {
+			$this->deletepost($params);
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 	
 	public function addPost($params = array())
     {	
+		$result = array();
 		
+		$this->api_url .= 'addpost.'.$this->api_response;
+		
+		try {
+			$this->addpost($params);
+		} catch (VboutException $ex) {
+			throw new VboutException($ex);
+        }
+		
+       return $result;
     }
 }
